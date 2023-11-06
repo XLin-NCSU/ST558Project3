1009f04e18087f644f8714170a9dbb84e03dc7c4
Xi Lin <xlin32@ncsu.edu>
2023-11-06 16:41:48 -0500

    Nov 6 update

---
+++ render.R
@@ -0,0 +1,14 @@
+rmarkdown::render("Project3.Rmd", output_format = "Edu1_2.md",
+                  params = list(Education = "2"))
+
+rmarkdown::render("Project3.Rmd", output_format = "Edu3.md",
+                  params = list(Education = "3"))
+
+rmarkdown::render("Project3.Rmd", output_format = "Edu4.md",
+                  params = list(Education = "4"))
+
+rmarkdown::render("Project3.Rmd", output_format = "Edu5.md",
+                  params = list(Education = "5"))
+
+rmarkdown::render("Project3.Rmd", output_format = "Edu6.md",
+                  params = list(Education = "6"))
